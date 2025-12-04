6318f06e9c23ed574559d1903615136edbe9bee6
Adam <adam@andyet.com>
2025-12-03 23:45:29 -0800

    ensure tag toggles update visual state

--- app/controllers/beads/issues/taggings_controller.rb
+++ app/controllers/beads/issues/taggings_controller.rb
@@ -8,11 +8,16 @@ class Beads::Issues::TaggingsController < ApplicationController
     # Ensure built-in beads tags exist in Fizzy
     ensure_beads_tags_exist
 
-    # BeadsIssue.tags returns an array of Tag objects, so we need to sort manually
-    @tagged_with = @card.tags.sort_by(&:title)
-    # Get all account tags not already on the card
-    tagged_ids = @tagged_with.map(&:id)
-    @tags = Current.account.tags.alphabetically.where.not(id: tagged_ids)
+    # Get tag titles currently on this beads issue
+    current_tag_titles = @card.tags.map(&:title)
+
+    # Get all account tags
+    all_tags = Current.account.tags.alphabetically
+
+    # Separate into tagged and untagged based on title match
+    @tagged_with = all_tags.select { |t| current_tag_titles.include?(t.title) }
+    @tags = all_tags.reject { |t| current_tag_titles.include?(t.title) }
+
     fresh_when etag: [ @tags, @card.tags ]
   end
 
@@ -23,11 +28,31 @@ class Beads::Issues::TaggingsController < ApplicationController
     # Check if this is a special tag (priority or type)
     if tag_title.match?(/^p[0-4]$/i)
       # Priority tag: p0, p1, p2, p3, p4
-      priority = tag_title[1].to_i
-      client.update(@issue.id, priority: priority)
-    elsif %w[bug feature epic chore task].include?(tag_title.downcase)
-      # Type tag
-      client.update(@issue.id, issue_type: tag_title.downcase)
+      current_priority_tag = "p#{@issue.priority}"
+
+      if tag_title == current_priority_tag
+        # Clicking current priority - reset to default (p2)
+        client.update(@issue.id, priority: 2)
+      else
+        # Change to new priority
+        priority = tag_title[1].to_i
+        client.update(@issue.id, priority: priority)
+      end
+    elsif %w[bug feature epic chore].include?(tag_title.downcase)
+      # Type tags (skip task - it's immutable)
+      has_type_in_labels = @issue.labels.include?(tag_title.downcase)
+
+      if has_type_in_labels
+        # Remove this type label
+        client.remove_label(@issue.id, tag_title.downcase)
+      else
+        # Remove old type labels
+        @issue.labels.select { |l| %w[bug feature epic chore].include?(l) }.each do |old_type|
+          client.remove_label(@issue.id, old_type)
+        end
+        # Add new type as label
+        client.add_label(@issue.id, tag_title.downcase)
+      end
     else
       # Regular label - check if we're toggling it
       if @issue.labels.include?(tag_title)
@@ -66,8 +91,8 @@ class Beads::Issues::TaggingsController < ApplicationController
         Current.account.tags.find_or_create_by!(title: tag_title)
       end
 
-      # Type tags: bug, feature, epic, chore, task
-      %w[bug feature epic chore task].each do |tag_title|
+      # Type tags: bug, feature, epic, chore (skip task - it's the default and can't be changed)
+      %w[bug feature epic chore].each do |tag_title|
         Current.account.tags.find_or_create_by!(title: tag_title)
       end
     end
