78a73ad7587feb740a05fc7b5497019fe23f6a2f
Adam <adam@andyet.com>
2025-12-05 05:26:42 -0800

    order done column by reverse chronological order

--- app/controllers/boards/columns/closeds_controller.rb
+++ app/controllers/boards/columns/closeds_controller.rb
@@ -3,13 +3,13 @@ class Boards::Columns::ClosedsController < ApplicationController
 
   def show
     if @board.beads_enabled?
-      # Show beads issues with status closed
+      # Show beads issues with status closed, most recently closed first
       issues = @board.beads_client.list(status: "closed")
       @beads_cards = issues.map do |data|
         issue = BeadsIssue.new(data)
         issue.board = @board
         issue
-      end
+      end.sort_by { |issue| issue.updated_at || Time.at(0) }.reverse
       @page = OpenStruct.new(records: @beads_cards, used?: @beads_cards.any?)
     else
       set_page_and_extract_portion_from @board.cards.closed.recently_closed_first.preloaded
